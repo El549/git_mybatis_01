a73113159e83e6684eee431e3c37cb493a1dae33
panda <248627806@qq.com>
2022-10-04 11:48:49 +0800

    v1

--- src/main/java/com/zlybl/test/Test.java
+++ src/main/java/com/zlybl/test/Test.java
@@ -9,14 +9,10 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException {
         //读取配置文件mybatis-config.xml
         InputStream config = Resources.getResourceAsStream("config/mybatis-config.xml");
         // 根据配置文件构建SqlSessionFactory
@@ -29,28 +25,7 @@ public class Test {
         List<Book> list = mapper.selectAll();
         //输出内容
         System.out.println(list);
-        Scanner in=new Scanner(System.in);
-        Book book=new Book();
-        System.out.println("需要添加加的书名为：");
-        book.setBname(in.next());
-        System.out.println("需要增加的书的作者为：");
-        book.setAuthor(in.next());
-        System.out.println("新书的价格为：");
-        book.setPrice(in.nextInt());
-        System.out.println("新书的出版社为：");
-        book.setPress(in.next());
-        System.out.println("新书的出版时间为(格式为yyyy-MM-dd hh:mm:ss)");
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        in.useDelimiter("\n");
-        String a=in.next();
-        Date date = sdf.parse(a);
-        book.setSaletime(date);
-        int flag= mapper.addBook(book);
-        if(flag>0){
-            System.out.println("添加成功，请在数据库中查看");
-        }else{
-            System.out.println("添加失败");
-        }
+
 
     }
 }
