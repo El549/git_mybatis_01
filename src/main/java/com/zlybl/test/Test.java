b698877f12695068585fd1c0a7d938a682b0f9d1
panda <248627806@qq.com>
2022-10-04 12:15:19 +0800

    v2.0 添加功能主菜单添加完成

--- src/main/java/com/zlybl/test/Test.java
+++ src/main/java/com/zlybl/test/Test.java
@@ -9,11 +9,14 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         //读取配置文件mybatis-config.xml
         InputStream config = Resources.getResourceAsStream("config/mybatis-config.xml");
         // 根据配置文件构建SqlSessionFactory
@@ -45,6 +48,30 @@ public class Test {
                     System.out.println(list);
                     break;
                 case 2:
+                    Scanner in=new Scanner(System.in);
+                    Book book=new Book();
+                    System.out.println("需要添加加的书名为：");
+                    book.setBname(in.next());
+                    System.out.println("需要增加的书的作者为：");
+                    book.setAuthor(in.next());
+                    System.out.println("新书的价格为：");
+                    book.setPrice(in.nextInt());
+                    System.out.println("新书的出版社为：");
+                    book.setPress(in.next());
+                    System.out.println("新书的出版时间为(格式为yyyy-MM-dd hh:mm:ss)");
+                    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    in.useDelimiter("\n");
+                    String a=in.next();
+                    Date date = sdf.parse(a);
+                    book.setSaletime(date);
+
+                    int a1= mapper.addBook(book);
+                    if(a1>0){
+                        System.out.println("添加成功，请在数据库中查看");
+                    }else{
+                        System.out.println("添加失败");
+                    }
+                    break;
                 case 3:
                     System.out.println("请输入需要删除的id号");
                     Integer id = scan.nextInt();
@@ -64,6 +91,7 @@ public class Test {
                 System.out.println("任意数字继续，输入0退出");
                 flag = scan.nextInt();
             }
+            sqlSession.commit();
         }
     }
 }
